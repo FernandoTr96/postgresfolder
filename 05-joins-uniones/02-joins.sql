9fc71fa5996638ed79c01aed8f8571b880a2a5ed
FernandoTr96 <fernando.tr96@outlook.es>
2024-01-18 22:52:10 -0600

    leccion de joins

--- 05-joins-uniones/02-joins.sql
+++ 05-joins-uniones/02-joins.sql
@@ -5,11 +5,14 @@ SELECT a.name as country, b.name as continent FROM country a INNER JOIN continen
 # LEFT OUTER JOINT: Muestra siempre los datos de A y considera los de la tabla B aun cuando en la tabla B no hay relacion mostrandolos como NULL
 # ejemplo: Resulta que la atlantida existe pero no esta en ningun continente esta en el mar, entonces se agrego como nuevo pais (Tabla A) pero como no hay 
 # continente (Tabla B) aparecera la atlantida como pais pero no su continente (NULL)
-
+SELECT a.name as country, b.name as continent FROM country a LEFT JOIN continent b on a.continent = b.code ORDER BY b.code DESC;
 
 # RIGHT OUTER JOIN: Muestra siempre la tabla B y si no hay relacion en la tabla A igual muetsra el registro de B pero el registro de A sera NULL (contrario al caso anterior)
+SELECT a.name as country, b.name as continent FROM country a RIGHT JOIN continent b on a.continent = b.code ORDER BY b.code DESC;
 
 # FULL OUTER JOIN: Basicamente muestra todos los casos anteriores, si hay relacion muestra los registros, si hay En A pero no en B muestralo pero B sera null y si hay en B pero no en A muestralo pero A sera Null
+SELECT a.name as Country,b.name as Continent FROM country a FULL OUTER JOIN continent b ON a.continent = b.code ORDER BY b.code DESC;
 
 # LEFT AND RIGHT OUTER JOIN EXCLUTION: Los join con exclusion son basicamente los mismos casos anteriores pero la exclusion viene en el where se agrega un where para mostrar 
 # o no los elementos NULL por ejemplo: where B.key IS NULL <-- dame todos los registros de A que no estuvieron en B
+SELECT a.name as Country,b.name as Continent FROM country a RIGHT JOIN continent b ON a.continent = b.code WHERE a.name IS NUll;
